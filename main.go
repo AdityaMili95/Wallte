fb0d4beba47749c4d557409aaea78adf10a61b96
Aditya MIli <aditya.mili@tokopedia.conm>
2017-11-02 10:12:40 +0700

    add map

--- main.go
+++ main.go
@@ -597,7 +597,7 @@ func handleTextMessage(event *linebot.Event, message *linebot.TextMessage) {
 
 	} else if msgCategory == PLAN {
 
-	} else if exist && data.Data.Last_Action != nil {
+	} else if exist && data.Data.Last_Action != nil && data.Data.Last_Action.Keyword != "" {
 		if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Ga kosong")).Do(); err != nil {
 			log.Print(err)
 		}
