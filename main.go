00dcf7b443f566d1469a481ddf830f39863aa681
Aditya MIli <aditya.mili@tokopedia.conm>
2017-11-03 09:34:42 +0700

    add datetime

--- main.go
+++ main.go
@@ -725,7 +725,7 @@ func handleAskDetail(event *linebot.Event, message *linebot.TextMessage, userID 
 		lastWeek := time.Now().AddDate(0, 0, -7)
 		now := time.Now()
 
-		log.Println(now.Format("2006-01-02T15:04"))
+		log.Println(time.Now().Format("2006-01-02T15:04"))
 
 		template := linebot.NewImageCarouselTemplate(
 
